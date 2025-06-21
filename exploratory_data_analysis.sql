f6269e5e04d924c540b4b0504f91d438541f98f0
Joshua <xyznathaniel@gmail.com>
2025-06-20 20:47:28 -0600

    Most layoff by company/industry

--- exploratory_data_analysis.sql
+++ exploratory_data_analysis.sql
@@ -13,3 +13,21 @@ SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY total_laid_off DESC;
+
+
+-- Date range
+
+SELECT MIN(`date`), MAX(`date`)
+FROM world_layoffs.layoffs_staging2;
+
+-- Companies and industries with the most layoff
+
+SELECT company, SUM(total_laid_off) AS sum_laid_off
+FROM world_layoffs.layoffs_staging2
+GROUP BY company 
+ORDER BY sum_laid_off DESC;
+
+SELECT industry, SUM(total_laid_off) AS sum_laid_off
+FROM world_layoffs.layoffs_staging2
+GROUP BY industry
+ORDER BY sum_laid_off DESC;
