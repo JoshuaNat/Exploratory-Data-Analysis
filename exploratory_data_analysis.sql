5ddd52af33d19da6edad3cd83f5d70bd4523663d
Joshua <xyznathaniel@gmail.com>
2025-06-20 21:29:45 -0600

    rolling total by month

--- exploratory_data_analysis.sql
+++ exploratory_data_analysis.sql
@@ -46,3 +46,24 @@ SELECT stage, SUM(total_laid_off) AS sum_laid_off
 FROM world_layoffs.layoffs_staging2
 GROUP BY stage
 ORDER BY sum_laid_off DESC;
+
+-- layoff by month
+
+SELECT SUBSTRING(`date`, 1, 7) AS `month`, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
+GROUP BY `month`
+ORDER BY `month`;
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`, 1, 7) AS `month`, SUM(total_laid_off) AS sum_laid_off
+FROM world_layoffs.layoffs_staging2
+WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
+GROUP BY `month`
+ORDER BY `month`
+)
+SELECT `month`, sum_laid_off, 
+SUM(sum_laid_off) OVER(ORDER BY `month`) AS rolling_total
+FROM Rolling_Total;
+
